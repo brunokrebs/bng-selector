35060b79a9c867c81871c5896663b5cb8bde1520
Bruno Krebs <bruno.krebs@tecsinapse.com.br>
2016-06-28 11:43:13 -0300

    Making component receive options.

--- src/bng-selector.js
+++ src/bng-selector.js
@@ -3,18 +3,14 @@
 
 	var bngSelectorController = function() {
 		var ctrl = this;
-
-		ctrl.options = [
-			'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'Curabitur'
-		];
-
 	};
 	bngSelectorController.$inject = [];
 
 	bngSelector.component('bngSelector', {
 		bindings: {
 			onSelect: '&',
-			onUnselect: '&'
+			onUnselect: '&',
+			options: '<'
 		},
 		templateUrl: 'bng-selector.html',
 		controller: bngSelectorController
