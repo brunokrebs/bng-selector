d9426ba69927f72251aa81e491717d55a7177c5e
Bruno Krebs <bruno.krebs@tecsinapse.com.br>
2016-06-28 18:20:23 -0300

    Better handling of filter closing process.

--- src/bng-selector.js
+++ src/bng-selector.js
@@ -43,9 +43,7 @@
 
 		ctrl.clear = function($event) {
 			ctrl.selected = null;
-			ctrl.term = '';
-			ctrl.filteredOptions = ctrl.options;
-			ctrl.showFilter = false;
+			closeFilter(true);
 			ctrl.onUnselect();
 			if ($event) {
 				$event.stopPropagation();
@@ -63,12 +61,15 @@
 			ctrl.showFilter = true;
 		}
 
-		function closeFilter(event) {
+		function closeFilter(avoidApply) {
 			componentElement.removeEventListener('keydown', handleKeyDown);
 			inputFilter.removeEventListener('keydown', handleKeyDown);
 			ctrl.showFilter = false;
 			ctrl.term = '';
-			$scope.$apply();
+			ctrl.filteredOptions = ctrl.options;
+			if (!avoidApply) {
+				$scope.$apply();
+			}
 		}
 
 		function handleKeyDown(event) {
